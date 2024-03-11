46199e8514f62ea1555070fd184de1f570bbbe42
Criscardozo94 <158121760+Criscardozo94@users.noreply.github.com>
2024-03-10 22:31:20 -0300

    first commit

---
+++ routes/libros.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+const Libro = require("../models/Libro");
+
+router.get("/", async (req, res) => {
+    try {
+    const libros = await Libro.find();
+    res.json(libros);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los libros" });
+        }
+    });
+
+    
+router.post("/", async (req, res) => {
+    try {
+        const nuevoLibro = new Libro(req.body);
+        await nuevoLibro.save();
+        res.json(nuevoLibro);
+    } catch (error) {
+            res.status(500).json({ error: "Error al crear el Libro" });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await Libro.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Libro eliminado correctamente' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al eliminar el Libro' });
+    }
+});
+
+module.exports = router;
